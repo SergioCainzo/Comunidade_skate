a5c193a9db13daf4b31c3f747004c5e3a3ad5636
Sergio Cainzo <97249973+SergioCainzo@users.noreply.github.com>
2022-10-25 16:16:47 -0300

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,8 @@
+// Variáveis com select
 const botaoMenu = document.querySelector('.cabecalho__menu');
 const menu = document.querySelector('.menu-lateral');
 
+// função para MUDAR a class do seletor
 botaoMenu.addEventListener('click', () => {
     menu.classList.toggle('menu-lateral--ativo');
 })
